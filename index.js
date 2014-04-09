8947764be49552425b120c91d5108cec247a2ecd
Kevin Martínez <n4utilius@gmail.com>
2014-04-09 07:22:21 -0500

    actualizar el readme

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 require('coffee-script/register');
 module.exports = require('./lib/cineteca.coffee')
 
-/*
-var print_all = function(err, movies) {
-    for (j in movies){
-      console.log(movies[j].title)
-    }
-  }
-
-cineteca.get_days( function(err, days) {
-    for (i in days){
-      cineteca.get_movies_by_day( print_all , days[i] )
-    }
-  })
-*/
